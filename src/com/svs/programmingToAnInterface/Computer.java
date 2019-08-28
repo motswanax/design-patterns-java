5ae8b8877de2e5e8eaf5d260b985e7341f55d664
Baiketli Dijo <baiketlid@gmail.com>
2019-08-28 16:11:19 +0200

    Demo of the principle of programming to an interface

---
+++ src/com/svs/programmingToAnInterface/Computer.java
@@ -0,0 +1,49 @@
+package com.svs.programmingToAnInterface;
+
+interface DisplayModule {
+    void display();
+}
+
+/*
+Concrete classes
+ */
+class Monitor implements DisplayModule {
+    @Override
+    public void display() {
+        System.out.println("Display through Monitor");
+    }
+}
+
+class Projector implements DisplayModule {
+    @Override
+    public void display() {
+        System.out.println("Display through Projector");
+    }
+}
+
+/**
+ * Client
+ */
+public class Computer {
+    private DisplayModule dm;
+
+    private void setDisplayModule(DisplayModule dm) {
+        this.dm = dm;
+    }
+
+    private void display() {
+        dm.display();
+    }
+
+    public static void main(String[] args) {
+        Computer cm = new Computer();
+        DisplayModule dm = new Monitor();
+        DisplayModule dm1 = new Projector();
+
+        cm.setDisplayModule(dm);
+        cm.display();
+        cm.setDisplayModule(dm1);
+        cm.display();
+    }
+
+}
